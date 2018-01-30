9ed45539d7e509e6862fbe22fb3fb8cd4f2b1dc4
Francisco Diaz <franciscojavier.diaz@i3television.es>
2018-01-30 17:59:17 +0100

    Added circular references company and user

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -6,21 +6,30 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLList
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
     name: 'Company',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString},
         name: { type: GraphQLString},
-        description: { type: GraphQLString}
-    }
+        description: { type: GraphQLString},
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/company/${parentValue.id}/users`)
+                    .then(res => {console.log(res);return res.data;})
+                    .catch(err=> console.log(err));
+            }
+        }        
+    })
 });
 
 const UserType = new GraphQLObjectType({
     name: 'User',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString},
         firstName: { type: GraphQLString},
         age: { type: GraphQLInt},
@@ -32,7 +41,7 @@ const UserType = new GraphQLObjectType({
                     .catch(err=> console.log(err));
             }
         }
-    }
+    })
 });
 
 const RootQuery = new GraphQLObjectType({
