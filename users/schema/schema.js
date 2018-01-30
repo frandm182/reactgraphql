bb48050fdf6a7a1805692f662b6c854b85af9487
Francisco Diaz <franciscojavier.diaz@i3television.es>
2018-01-30 16:10:51 +0100

    added async user service

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,4 +1,5 @@
 const graphql = require('graphql');
+const axios = require('axios');
 const _ = require('lodash');
 
 const {
@@ -8,10 +9,6 @@ const {
     GraphQLSchema
 } = graphql;
 
-const users = [
-    { id: '23', firstName: 'Bill', age: 20 },
-    { id: '47', firstName: 'Samantha', age: 21 }
-];
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -29,7 +26,9 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: { id: { type: GraphQLString } },
             resolve(parentValue, args) {
-                return _.find(users, { id: args.id })
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then(res => res.data)
+                    .catch(err=> console.log(err));
             }
         }
     }
