4febe5c25f773d28df3aab537b2ff1396ac4f3d0
Francisco Diaz <franciscojavier.diaz@i3television.es>
2018-01-31 13:28:24 +0100

    Adding collection styles

--- users/client/components/SongList.js
+++ users/client/components/SongList.js
@@ -6,7 +6,7 @@ class SongList extends Component {
     renderSongs() {
         return this.props.data.songs.map(song => {
             return (
-            <li key={song.title}>
+            <li key={song.id} className="collection-item">
                 {song.title}
             </li>
             );
@@ -16,13 +16,18 @@ class SongList extends Component {
         if(this.props.data.loading) {  
             return (<div>Loading...</div>); 
         }
-       return (<ul>{this.renderSongs()}</ul>);
+       return (
+            <ul className="collection">
+                {this.renderSongs()}
+            </ul>
+        );
     }
 }
 
 const query = gql`
     {
         songs {
+            id
             title
         }
     }
