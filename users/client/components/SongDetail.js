b27f8d5bffedc12bc131cebf179135124ed5b64b
Francisco Diaz <franciscojavier.diaz@i3television.es>
2018-01-31 15:44:41 +0100

    Added fetch song in react-router

--- users/client/components/SongDetail.js
+++ users/client/components/SongDetail.js
@@ -3,10 +3,11 @@ import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link, hashHistory } from 'react-router';
 
-import query from '../queries/fetchSongs';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
     render() {
+        console.log(this.props)
       return (
           <div>              
               <h3>Song detail</h3>             
@@ -15,4 +16,6 @@ class SongDetail extends Component {
     }
 }
 
-export default SongDetail;
+export default graphql(fetchSong, {
+    options: (props) => { return { variables: { id: props.params.id }}}
+})(SongDetail);
